2c74aa25252df24783702fb2e0191ae6ab22f787
Mateusz <mateuszstanczak17@gmail.com>
2024-01-14 00:16:12 +0100

    first commit

---
+++ example-app/app/Http/Controllers/PeopleController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\People;
+class PeopleController extends Controller
+{
+    public function index()
+{
+    return People::all();
+}
+
+public function store(Request $request)
+{
+    $people = People::create($request->all());
+
+    return response()->json($people, 201);
+}
+
+public function show(People $people)
+{
+    return response()->json($people);
+}
+
+public function update(Request $request, People $people)
+{
+    $people->update($request->all());
+
+    return response()->json($people);
+}
+
+public function destroy(People $people)
+{
+    $people->delete();
+
+    return response()->json(null, 204);
+}
+}
